3a35a95545e83e7507a7c1d84d92c5116e5ac285
DASHU <385321165@qq.com>
2026-02-04 17:54:07 +0800

    add methods to NesignerInterface

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,11 @@ export interface NesignerInterface {
     nip44Decrypt(pubkey: string, ciphertext: string): Promise<string | null>;
     sign(eventId: string): Promise<string | null>;
     close(): Promise<void>;
+
+    updateKey(pinCode: string, key: string): Promise<number>;
+    removeKey(pinCode: string): Promise<number>;
+    ping(): Promise<number | null>;
+    echo(pinCode: string, msgContent: string): Promise<string | null>;
 }
 
 export enum MsgType {
